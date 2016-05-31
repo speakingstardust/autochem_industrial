2e457949f8dbd5567cfed946f3649193d993963c
speakingstardust <josh@autochemky.com>
2016-05-31 18:31:36 -0400

    finished with model tests and added has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
 
+	has_secure_password
+
+	before_save	{ self.email = email.downcase }
+
 	VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
 	validates :first_name, presence: true, length: { maximum: 50 }
 	validates :last_name, presence: true, length: { maximum: 50 }
-	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	validates :password, presence: true, length: { minimum: 6 }
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
 	def setup
-		@user = User.new(first_name: "Example", last_name: "User", email: "user@example.com")
+		@user = User.new(first_name: "Example", last_name: "User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
 	end
 
 	test "should be valid" do
@@ -55,4 +55,22 @@ class UserTest < ActiveSupport::TestCase
 			assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
 		end
 	end
+
+	test "email should be unique" do
+		duplicate_user = @user.dup
+		duplicate_user.email = duplicate_user.email.upcase
+		@user.save
+		assert_not duplicate_user.valid?
+	end
+
+	test "password should be present (non-blank)" do
+		@user.password = @user.password_confirmation = " " * 6
+		assert_not @user.valid?
+	end
+
+	test "password should have a minimum length" do
+		@user.password = @user.password_confirmation = "a" * 5
+		assert_not @user.valid?
+	end
+
 end
